8b52b1f220d30a53e607e60e62acb89737ae8381
databox <headprogrammingczar@gmail.com>
2013-05-06 20:12:04 -0400

    frist!

---
+++ Control/Comonad/Decept.hs
@@ -0,0 +1,14 @@
+module Control.Comonad.Decept
+( module Control.Comonad.Trans.Class
+, module Control.Comonad.Trans.Env
+, module Control.Comonad.Trans.Identity
+, module Control.Comonad.Trans.Store
+, module Control.Comonad.Trans.Traced
+) where
+
+import Control.Comonad.Trans.Class
+import Control.Comonad.Trans.Env
+import Control.Comonad.Trans.Identity
+import Control.Comonad.Trans.Store
+import Control.Comonad.Trans.Traced
+
